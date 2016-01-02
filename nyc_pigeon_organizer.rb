34ef07c0f79297f25389e1cdb420bd35ed8d8826
franco84 <franco84@gmail.com>
2016-01-02 20:11:28 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  birds = {}
+  keys = data[:lives].values.flatten.uniq
+  keys.each do |name|
+  birds[name] = {}
+  end
+
+  keys.each do |name|
+    data.each do |types, data|
+      qualities = []
+      data.each do |quality, names|
+        if names.include?(name)
+          qualities << quality.to_s
+          birds[name][types] = qualities
+        end
+      end
+    end
+  end
+  birds
 end
